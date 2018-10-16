2902971da74a095bb081a9204f0a7da1f55b0b6f
Lamuel27 <samrelliner@gmail.com>
2018-10-16 15:00:01 -0500

    jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -79,3 +79,115 @@ var questions = [{
   image3:"assets/images/John-Hammond-Damn.gif"
 }];
 
+
+
+var game = {
+    questions:questions,
+    currentQuestion:0,
+    counter:countStartNumber,
+    correct:0,
+    incorrect:0,
+    countdown: function(){
+      game.counter--;
+      $('#counter-number').html(game.counter);
+  
+      if (game.counter === 0){
+        console.log('TIME UP');
+        game.timeUp();
+      }
+    },
+    
+    loadQuestion: function(){
+      timer = setInterval(game.countdown, 1000);
+      panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+      $('h2').css('color','white');
+      for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+        panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+        $('.answer-button').css('color','white');
+      }
+    },
+    //load next question
+    nextQuestion: function(){
+      game.counter = countStartNumber;
+      $('#counter-number').html(game.counter);
+      game.currentQuestion++;
+      game.loadQuestion();
+    },
+    timeUp: function (){
+      clearInterval(timer);
+      $('#counter-number').html(game.counter);
+  //Display when you are out of time
+      panel.html('<h2>Out of Time!</h2>');
+      panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+      $('h2').css('color','white');
+      panel.append('<img src="' + questions[this.currentQuestion].image3 + '" />');
+  
+      if (game.currentQuestion === questions.length - 1){
+        setTimeout(game.results, 3 * 1000);
+      } else {
+        setTimeout(game.nextQuestion, 3 * 1000);
+      }
+    },
+    results: function() {
+      clearInterval(timer);
+  //display your results
+      panel.html("<h2>All done, here's how you did!</h2>");
+      $('h2').css('color','white');
+      $('#counter-number').html(game.counter);
+      $('#counter-number').css('color','white');
+      panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+      $('h3').css('color','white');
+      panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
+      $('h3').css('color','white');
+      panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+      $('h3').css('color','white');
+      panel.append('<br><button id="start-over">Start Over?</button>');
+      $('#start-over').css('color','white');
+    },
+    clicked: function(e) {
+      clearInterval(timer);
+  
+      if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+        this.answeredCorrectly();
+      } else {
+        this.answeredIncorrectly();
+      }
+    },
+    answeredIncorrectly: function() {
+      game.incorrect++;
+      clearInterval(timer);
+      panel.html('<h2>Ah ah ah, that is incorrect!</h2>');
+      $('h2').css('color','white');
+      panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+      $('h3').css('color','white');
+      panel.append('<img src="' + questions[game.currentQuestion].image2 + '" />');
+  
+      if (game.currentQuestion === questions.length - 1){
+        setTimeout(game.results, 3 * 1000);
+      } else {
+        setTimeout(game.nextQuestion, 3 * 1000);
+      }
+    },
+    //pull gif if answered correctly
+    answeredCorrectly: function(){
+      clearInterval(timer);
+      game.correct++;
+      panel.html('<h2>Correct!</h2>');
+      $('h2').css('color','white');
+      panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+  //length of time to show gif
+      if (game.currentQuestion === questions.length - 1){
+        setTimeout(game.results, 5 * 1000);
+      } else {
+        setTimeout(game.nextQuestion, 5 * 1000);
+      }
+    },
+    reset: function(){
+      this.currentQuestion = 0;
+      this.counter = countStartNumber;
+      this.correct = 0;
+      this.incorrect = 0;
+      this.loadQuestion();
+    }
+  };
+  
