b700729e1a32f5bbd0230a02b6537a79947996a9
Lamuel27 <samrelliner@gmail.com>
2018-10-17 18:43:31 -0500

    font size

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -167,9 +167,9 @@ var game = {
       panel.append('<img src="' + questions[game.currentQuestion].image2 + '" />');
   
       if (game.currentQuestion === questions.length - 1){
-        setTimeout(game.results, 3 * 1000);
+        setTimeout(game.results, 6 * 1000);
       } else {
-        setTimeout(game.nextQuestion, 3 * 1000);
+        setTimeout(game.nextQuestion, 6 * 1000);
       }
     },
     //pull gif if answered correctly
